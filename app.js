858230c6ed50e5f02da826e6bf562fa35edf8bf6
Iugai Olga <espritolka@gmail.com>
2019-03-16 17:50:51 +0300

    find

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 const MongoClient = require("mongodb").MongoClient;
-   
+   
 const url = "mongodb://localhost:27017/";
 const mongoClient = new MongoClient(url, { useNewUrlParser: true });
- 
-let users = [{name: "Bob", age: 34} , {name: "Alice", age: 21}, {name: "Tom", age: 45}];
- 
+ 
 mongoClient.connect(function(err, client){
-      
-    const db = client.db("usersdb");
-    const collection = db.collection("users");
-     
-    collection.insertMany(users, function(err, results){
-              
-        console.log(results);
-        client.close();
-    });
+      
+    const db = client.db("usersdb");
+    const collection = db.collection("users");
+ 
+    if(err) return console.log(err);
+      
+    collection.find().toArray(function(err, results){
+                 
+        console.log(results);
+        client.close();
+    });
 });
