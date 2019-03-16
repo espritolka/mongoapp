af0799ea262edecd89c4587e55cc5586786ea80f
Iugai Olga <espritolka@gmail.com>
2019-03-16 18:04:29 +0300

    delete users collection

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 const MongoClient = require("mongodb").MongoClient;
-   
+   
 const url = "mongodb://localhost:27017/";
 const mongoClient = new MongoClient(url, { useNewUrlParser: true });
-  
+  
 mongoClient.connect(function(err, client){
-     
-    if(err) return console.log(err);
-      
-    const db = client.db("usersdb");
-    db.collection("users").findOneAndDelete({age: 21}, function(err, result){
-              
-        console.log(result);
-        client.close();
-    });
+     
+    if(err) return console.log(err);
+      
+    const db = client.db("usersdb");
+    db.collection("users").drop(function(err, result){
+              
+        console.log(result);
+        client.close();
+    });
 });
