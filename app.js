ceba4f12d6d5a225ffbafe08355acb9ca5e6bfa4
Iugai Olga <espritolka@gmail.com>
2019-03-16 18:22:09 +0300

    test2

--- app.js
+++ app.js
@@ -2,21 +2,18 @@ const MongoClient = require("mongodb").MongoClient;
    
 const url = "mongodb://localhost:27017/";
 const mongoClient = new MongoClient(url, { useNewUrlParser: true });
-  
+ 
 mongoClient.connect(function(err, client){
      
     if(err) return console.log(err);
       
     const db = client.db("usersdb");
     const col = db.collection("users");
-    col.findOneAndUpdate(
-        {name: "Bob"},              // критерий выборки
-        { $set: {name: "Sam"}},     // параметр обновления
-        {                           // доп. опции обновления    
-            returnOriginal: false
-        },
+    col.updateMany(
+        {name: "Sam"},              // критерий фильтрации
+        { $set: {name: "Bob"}},     // параметр обновления
         function(err, result){
-                 
+                  
             console.log(result);
             client.close();
         }
