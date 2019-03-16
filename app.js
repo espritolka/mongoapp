5583d4efbcae994d251ff9865c047b3c319a828b
Iugai Olga <espritolka@gmail.com>
2019-03-16 17:42:40 +0300

    insertMany

--- app.js
+++ app.js
@@ -3,17 +3,16 @@ const MongoClient = require("mongodb").MongoClient;
 const url = "mongodb://localhost:27017/";
 const mongoClient = new MongoClient(url, { useNewUrlParser: true });
  
+let users = [{name: "Bob", age: 34} , {name: "Alice", age: 21}, {name: "Tom", age: 45}];
+ 
 mongoClient.connect(function(err, client){
       
     const db = client.db("usersdb");
     const collection = db.collection("users");
-    let user = {name: "Tom", age: 23};
-    collection.insertOne(user, function(err, result){
-          
-        if(err){ 
-            return console.log(err);
-        }
-        console.log(result.ops);
+     
+    collection.insertMany(users, function(err, results){
+              
+        console.log(results);
         client.close();
     });
 });
