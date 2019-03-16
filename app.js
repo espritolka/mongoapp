0a5f81b6ba44bd8bc5cfef3267434a37f9a8c091
Iugai Olga <espritolka@gmail.com>
2019-03-16 18:10:26 +0300

    test findOneAndUpdate

--- app.js
+++ app.js
@@ -3,14 +3,23 @@ const MongoClient = require("mongodb").MongoClient;
 const url = "mongodb://localhost:27017/";
 const mongoClient = new MongoClient(url, { useNewUrlParser: true });
   
+let users = [{name: "Bob", age: 34} , {name: "Alice", age: 21}, {name: "Tom", age: 45}]; 
 mongoClient.connect(function(err, client){
      
     if(err) return console.log(err);
       
     const db = client.db("usersdb");
-    db.collection("users").drop(function(err, result){
-              
-        console.log(result);
-        client.close();
+    const col = db.collection("users");
+    col.insertMany(users, function(err, results){
+             
+        col.findOneAndUpdate(
+            {age: 21}, // критерий выборки
+            { $set: {age: 25}}, // параметр обновления
+            function(err, result){
+                 
+                console.log(result);
+                client.close();
+            }
+        );
     });
 });
