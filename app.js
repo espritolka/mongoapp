5247d92382a5bbef569853883d5da5bb1a62cf7d
Iugai Olga <espritolka@gmail.com>
2019-03-19 23:02:13 +0300

    test mongoos api

--- app.js
+++ app.js
@@ -1,28 +1,74 @@
 const mongoose = require("mongoose");
+const express = require("express");
 const Schema = mongoose.Schema;
- 
-mongoose.connect("mongodb://localhost:27017/usersdb", { useNewUrlParser: true });
+const app = express();
+const jsonParser = express.json();
  
 const userScheme = new Schema({name: String, age: Number}, {versionKey: false});
 const User = mongoose.model("User", userScheme);
  
-User.find({}, function(err, docs){
-    mongoose.disconnect();
-     
+app.use(express.static(__dirname + "/public"));
+ 
+mongoose.connect("mongodb://localhost:27017/usersdb", { useNewUrlParser: true }, function(err){
     if(err) return console.log(err);
+    app.listen(3000, function(){
+        console.log("Сервер ожидает подключения...");
+    });
+});
+  
+app.get("/api/users", function(req, res){
+        
+    User.find({}, function(err, users){
+ 
+        if(err) return console.log(err);
+        res.send(users)
+    });
+});
+ 
+app.get("/api/users/:id", function(req, res){
+         
+    const id = req.params.id;
+    User.findOne({_id: id}, function(err, user){
+          
+        if(err) return console.log(err);
+        res.send(user);
+    });
+});
+    
+app.post("/api/users", jsonParser, function (req, res) {
+        
+    if(!req.body) return res.sendStatus(400);
+        
+    const userName = req.body.name;
+    const userAge = req.body.age;
+    const user = new User({name: userName, age: userAge});
+        
+    user.save(function(err){
+        if(err) return console.log(err);
+        res.send(user);
+    });
+});
      
-    console.log(docs);
+app.delete("/api/users/:id", function(req, res){
+         
+    const id = req.params.id;
+    User.findByIdAndDelete(id, function(err, user){
+                
+        if(err) return console.log(err);
+        res.send(user);
+    });
 });
-User.findOneAndDelete({name:"Bill"}, function(err, doc){
-    mongoose.disconnect();
-     
-    if(err) return console.log(err);
-     
-    console.log("Удален пользователь ", doc);
-});
-User.updateOne({name: "Tom"}, {name: "Tom Smith"}, function(err, result){
-     
-    mongoose.disconnect();
-    if(err) return console.log(err);
-    console.log(result);
+    
+app.put("/api/users", jsonParser, function(req, res){
+         
+    if(!req.body) return res.sendStatus(400);
+    const id = req.body.id;
+    const userName = req.body.name;
+    const userAge = req.body.age;
+    const newUser = {age: userAge, name: userName};
+     
+    User.findOneAndUpdate({_id: id}, newUser, {new: true}, function(err, user){
+        if(err) return console.log(err); 
+        res.send(user);
+    });
 });
