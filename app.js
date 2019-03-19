3c724166835b434a00020b55e45a473dd993ed1d
Iugai Olga <espritolka@gmail.com>
2019-03-19 22:59:53 +0300

    test metods

--- app.js
+++ app.js
@@ -6,10 +6,23 @@ mongoose.connect("mongodb://localhost:27017/usersdb", { useNewUrlParser: true })
 const userScheme = new Schema({name: String, age: Number}, {versionKey: false});
 const User = mongoose.model("User", userScheme);
  
-User.find({"age":34}, function(err, docs){
+User.find({}, function(err, docs){
     mongoose.disconnect();
      
     if(err) return console.log(err);
      
     console.log(docs);
+});
+User.findOneAndDelete({name:"Bill"}, function(err, doc){
+    mongoose.disconnect();
+     
+    if(err) return console.log(err);
+     
+    console.log("Удален пользователь ", doc);
+});
+User.updateOne({name: "Tom"}, {name: "Tom Smith"}, function(err, result){
+     
+    mongoose.disconnect();
+    if(err) return console.log(err);
+    console.log(result);
 });
