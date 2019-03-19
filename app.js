c843b6e9a3e16f5c11d48be3b2f524e3a06f4a7a
Iugai Olga <espritolka@gmail.com>
2019-03-19 22:51:34 +0300

    test mongoos

--- app.js
+++ app.js
@@ -1,93 +1,32 @@
-const express = require("express");
-const MongoClient = require("mongodb").MongoClient;
-const objectId = require("mongodb").ObjectID;
-   
-const app = express();
-const jsonParser = express.json();
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
  
-const mongoClient = new MongoClient("mongodb://localhost:27017/", { useNewUrlParser: true });
+// подключение
+mongoose.connect("mongodb://localhost:27017/usersdb", { useNewUrlParser: true });
  
-let dbClient;
+// установка схемы
+const userScheme = new Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength:3,
+        maxlength:20
+    },
+    age: {
+        type: Number,
+        required: true,
+        min: 1,
+        max:100
+    }
+},
+{ versionKey: false });
+const User = mongoose.model("User", userScheme);
+const user = new User({name: "Tom", age: 34});
  
-app.use(express.static(__dirname + "/public"));
- 
-mongoClient.connect(function(err, client){
-    if(err) return console.log(err);
-    dbClient = client;
-    app.locals.collection = client.db("usersdb").collection("users");
-    app.listen(3000, function(){
-        console.log("Сервер ожидает подключения...");
-    });
-});
- 
-app.get("/api/users", function(req, res){
-        
-    const collection = req.app.locals.collection;
-    collection.find({}).toArray(function(err, users){
-
-        if(err) return console.log(err);
-        res.send(users)
-    });
+user.save(function(err){
+    mongoose.disconnect();
      
-});
-app.get("/api/users/:id", function(req, res){
-        
-    const id = new objectId(req.params.id);
-    const collection = req.app.locals.collection;
-    collection.findOne({_id: id}, function(err, user){
-               
-        if(err) return console.log(err);
-        res.send(user);
-    });
-});
-   
-app.post("/api/users", jsonParser, function (req, res) {
-       
-    if(!req.body) return res.sendStatus(400);
-       
-    const userName = req.body.name;
-    const userAge = req.body.age;
-    const user = {name: userName, age: userAge};
-       
-    const collection = req.app.locals.collection;
-    collection.insertOne(user, function(err, result){
-               
-        if(err) return console.log(err);
-        res.send(user);
-    });
-});
-    
-app.delete("/api/users/:id", function(req, res){
-        
-    const id = new objectId(req.params.id);
-    const collection = req.app.locals.collection;
-    collection.findOneAndDelete({_id: id}, function(err, result){
-               
-        if(err) return console.log(err);    
-        let user = result.value;
-        res.send(user);
-    });
-});
-   
-app.put("/api/users", jsonParser, function(req, res){
-        
-    if(!req.body) return res.sendStatus(400);
-    const id = new objectId(req.body.id);
-    const userName = req.body.name;
-    const userAge = req.body.age;
-       
-    const collection = req.app.locals.collection;
-    collection.findOneAndUpdate({_id: id}, { $set: {age: userAge, name: userName}},
-         {returnOriginal: false },function(err, result){
-               
-        if(err) return console.log(err);     
-        const user = result.value;
-        res.send(user);
-    });
-});
- 
-// прослушиваем прерывание работы программы (ctrl-c)
-process.on("SIGINT", () => {
-    dbClient.close();
-    process.exit();
+    if(err) return console.log(err);
+     
+    console.log("Сохранен объект user", user);
 });
