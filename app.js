e6d37807a4b0daca7b50fd6b46cd310226fa5d3a
Iugai Olga <espritolka@gmail.com>
2019-03-19 22:57:15 +0300

    find

--- app.js
+++ app.js
@@ -1,32 +1,15 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
  
-// подключение
 mongoose.connect("mongodb://localhost:27017/usersdb", { useNewUrlParser: true });
  
-// установка схемы
-const userScheme = new Schema({
-    name: {
-        type: String,
-        required: true,
-        minlength:3,
-        maxlength:20
-    },
-    age: {
-        type: Number,
-        required: true,
-        min: 1,
-        max:100
-    }
-},
-{ versionKey: false });
+const userScheme = new Schema({name: String, age: Number}, {versionKey: false});
 const User = mongoose.model("User", userScheme);
-const user = new User({name: "Tom", age: 34});
  
-user.save(function(err){
+User.find({"age":34}, function(err, docs){
     mongoose.disconnect();
      
     if(err) return console.log(err);
      
-    console.log("Сохранен объект user", user);
+    console.log(docs);
 });
